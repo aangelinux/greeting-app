c2fc648d3ca74c030279379f97dc76deb3d0e4e8
Angelina Larsson <al227bn@student.lnu.se>
2025-09-07 16:24:17 +0200

    Add style to the component

--- src/js/components/greeting-app.js
+++ src/js/components/greeting-app.js
@@ -1,60 +1,67 @@
 /**
  * The greeting-app web component.
+ * 
+ * @file components/greeting-app
+ * @module Generates a random greeting for the user.
  */
 
 const template = document.createElement('template')
 template.innerHTML = `
-  <style>
-    body {
-      margin: 0;
-      height: 100vh;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      background-color: #f0f4f8;
-      font-family: 'Georgia';
-    }
-
+<style>
     .greeting-app {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      color: darkblue;
-      font-size: 1.5rem;
-      font-family: 'PressStart2P';
-      margin: 7px;
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        background-color: #f4f1de;
+        color: #e07a5f;
+        font-size: 1.4rem;
+        font-family: 'Monaco', monospace;
+        font-weight: bold;
+        margin: 0;
     }
 
-  .greeting-app input {
-    padding: 10px;
-    font-size: 1rem;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    width: 200px;
-  }
+    .greeting-app h1 {
+        text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000, 1px 1px 0 #000;
+    }
 
-  .greeting-app button {
-    padding: 10px 15px;
-    font-size: 1rem;
-    color: white;
-    background-color: darkblue;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-  }
+    .greeting-app input {
+        margin-top: 10px;
+        padding: 10px;
+        font-size: 1rem;
+        border: 2px solid black;
+        border-radius: 5px;
+        width: 200px;
+    }
 
-  .greeting-app button:hover {
-    background-color: #00008b;
-  }
-  </style>
+    .greeting-app button {
+        padding: 10px 15px;
+        font-family: 'Monaco', monospace;
+        font-size: 1rem;
+        color: white;
+        background-color: #e07a5f;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+    }
 
-  <div class="greeting-app">
+    .greeting-app button:hover {
+        background-color: #e1b8abff;
+    }
+
+    .greeting-app h3 {
+        margin-top: 40px;
+    }
+</style>
+
+<div class="greeting-app">
     <h1>Welcome to the greeting app!</h1>
     <label for="name">Please enter your name:</label>
     <input type="text" id="name" name="name"><br><br>
-    <button type="button">Generate greeting!</button>
-    <h3><h3>
-  </div>
+    <button type="button">GENERATE GREETING</button>
+    <h3></h3>
+</div>
 `
 
 customElements.define('greeting-app',
@@ -74,6 +81,8 @@ customElements.define('greeting-app',
         this.inputBox = this.shadowRoot.querySelector('input')
         this.button = this.shadowRoot.querySelector('button')
         this.greeting = this.shadowRoot.querySelector('h3')
+
+        this.abortController = new AbortController()
     }
 
     /**
@@ -82,36 +91,41 @@ customElements.define('greeting-app',
     connectedCallback () {
         this.inputBox.select()
 
-        this.button.addEventListener('click', () => this.runProgram())
+        this.button.addEventListener('click', () => this.runProgram(), { signal: this.abortController.signal })
         this.inputBox.addEventListener('keypress', (event) => {
             if (event.key === "Enter") {
                 this.runProgram()
             }
-        })
+        }, { signal: this.abortController.signal })
     }
 
     /**
      * Called when the element is removed from the DOM.
      */
     disconnectedCallback () {
-        this.button.removeEventListener('click')
-        this.inputBox.removeEventListener('keypress')
+        this.abortController.abort()
     }
 
     /**
-     * Reads the input and generates a greeting.
+     * Reads the input and generates a random greeting.
      */
     generateGreeting () {
-        const userName = this.inputBox.value
+        const userName = this.inputBox.value.trim()
+        if (!userName) {
+            return 'Please enter a name.'
+        }
 
         const greetings = {
             Swedish: 'Goddag',
             English: 'Good day',
-            French: 'Bonne journée',
+            French: 'Bonjour',
             Spanish: 'Buen día',
             Tagalog: 'Magandang araw',
             German: 'Guten tag',
-            Australian: `G'day`
+            Polish: 'Dzień dobry',
+            Italian: 'Buongiorno',
+            Australian: `G'day`,
+            Cowboyian: 'Howdy',
         }
 
         const keys = Object.keys(greetings)
@@ -122,19 +136,11 @@ customElements.define('greeting-app',
     }
 
     /**
-     * Displays the greeting on the page.
-     * 
-     * @param {string} - The greeting to be displayed.
-     */
-    displayGreeting (greeting) {
-        this.greeting.innerText = greeting
-    }
-
-    /**
-     * Runs the program.
+     * Gets a greeting and displays it on the page.
      */
     runProgram () {
         const greeting = this.generateGreeting()
-        this.displayGreeting(greeting)
+
+        this.greeting.innerText = greeting
     }
 })
